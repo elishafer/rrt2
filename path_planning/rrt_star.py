4f4a7f2d58d30bea091392f0ac2aaa290858b142
Elisei <elishafer@users.noreply.github.com>
2019-11-27 12:06:08 +0200

    Delint

--- path_planning/rrt_star.py
+++ path_planning/rrt_star.py
@@ -18,15 +18,16 @@ def distance(ox, oy, x, y):
     return sqrt((ox - x) ** 2 + (oy - y) ** 2)
 
 
-def mynorm(a,b=None):
-    if b == None:
-        b = (0,0)
-    return distance(a[0],a[1],b[0],b[1])
+def mynorm(a, b=None):
+    if b is None:
+        b = (0, 0)
+    return distance(a[0], a[1], b[0], b[1])
+
 
 class RrtStar:
 
     def __init__(self, start, goal, c_space_bounds, obstacle_list, max_iterations=1500,
-                 max_extend=4.0, goal_sample_rate=5):
+                 max_extend=4.0, goal_sample_rate=1):
         self.start = start
         self.goal = goal
         self.max_iterations = max_iterations
@@ -77,14 +78,13 @@ class RrtStar:
         # https://stackoverflow.com/questions/1073336/circle-line-segment-collision-detection-algorithm
         d = x_new - x_old
         for (ox, oy, r) in obstacle_list:
-            f = x_old - np.array([ox,oy])
+            f = x_old - np.array([ox, oy])
             a = d.dot(d)
             b = 2 * f.dot(d)
             c = f.dot(f) - r * r
             if b * b - 4 * a * c >= 0:
                 return False
         return True
-
 
     def draw_graph(self, rnd=None):  # pragma: no cover
         """
@@ -96,7 +96,7 @@ class RrtStar:
         for node in self.nodes:
             if node.parent is not None:
                 plt.plot([node.x[1], self.nodes[node.parent].x[1]], [
-                         node.x[0], self.nodes[node.parent].x[0]], "-g")
+                    node.x[0], self.nodes[node.parent].x[0]], "-g")
 
         circles = []
         fig = plt.gcf()
@@ -115,7 +115,7 @@ class RrtStar:
                   self.c_space_bounds[0][0], self.c_space_bounds[0][1]])
         plt.grid(True)
 
-    def algo(self,animation=False):
+    def algo(self, animation=False):
 
         eta = self.max_extend * 8.0
         c_space_size = [x[1]-x[0] for x in self.c_space_bounds]
@@ -133,7 +133,7 @@ class RrtStar:
                 for i, x_near in enumerate(X_near):
                     # Check for lowest cost node to connect to.
                     if not self.is_path_collision_free(x_near.x, x_new, self.obstacle_list):
-                        #Collision check in middle between 2 nodes
+                        # Collision check in middle between 2 nodes
                         continue
                     # c_i = x_near.cost + mynorm(x_new-x_near.x)
                     c_i = self.cost_func(x_near.cost, x_new, x_near.x)
@@ -151,14 +151,14 @@ class RrtStar:
                 for i, x_near in enumerate(X_near):
                     # Rewire adjacent nodes
                     if not self.is_path_collision_free(x_near.x, x_new, self.obstacle_list):
-                        #Collision check in middle between 2 nodes
+                        # Collision check in middle between 2 nodes
                         continue
                     # c_i = new_node.cost + mynorm(x_new - x_near.x)
                     c_i = self.cost_func(new_node.cost, x_near.x, x_new)
                     if c_i < x_near.cost:
-                        self.nodes[nearinds[i]].parent = len(self.nodes)-1
+                        self.nodes[nearinds[i]].parent = len(self.nodes) - 1
 
-            if animation:# and i%5:
+            if animation:  # and i%5:
                 self.draw_graph(x_rand)
 
         path = [[self.goal[0], self.goal[1]]]
@@ -208,17 +208,15 @@ class RrtStar:
         new_cost = prev_cost + dist + safety_cost
         return new_cost
 
-
     def safety_cost_func(self, x, y):
 
         for (ox, oy, ro) in self.obstacle_list:
             # TODO change 123 to variable
             for r in [1, 2, 3]:
                 if (ox + x) ** 2 + (oy + y) ** 2 < ((ro + r) ** 2):
-                    return (4-r)
+                    return 4 - r
         return 0
 
-# def circles_intersect()
 
 def save_path(robot_path, dir_path="path.pkl"):
     robot_path.reverse()
@@ -234,14 +232,13 @@ class Node():
         self.cost = 0.0
 
 
-def  ros_path_planning_node(s, t, obstacle_list, step_size):
-
+def ros_path_planning_node(s, t, obstacle_list, step_size):
     # Note here all coordinates are in ned
     # We add padding of 10m around the start and target
     # Maybe should change this to use library.
     r_e = 6366707.0
-    pad = 57.3*10.0/r_e
-    step_size_rad = 57.3 * step_size/r_e
+    pad = 57.3 * 10.0 / r_e
+    step_size_rad = 57.3 * step_size / r_e
     c_space_bounds = [(min(s[0], t[0]) - pad, max(s[0], t[0]) + pad),
                       (min(s[1], t[1]) - pad, max(s[1], t[1]) + pad)]
     rrt = RrtStar(start=s, goal=t,
@@ -253,21 +250,20 @@ def  ros_path_planning_node(s, t, obstacle_list, step_size):
     return path, rrt
 
 
-def main(goal=[-15,80.0]):
+def main(goal=[-15, 80.0]):
     print("start " + __file__)
     goal = [32.826240, 34.956214]
     start = [32.827320, 34.954897]
     # goal = [50, 50]
     # start = [5, 5]
 
-    # ====Search Path with RRT====
-    with open('obstacle_list.json') as obstacle_file:
-        obstacle_dict = json.load(obstacle_file)
-        # obstacle_list = obstacle_dict['mlo_3']
-        obstacle_list = [[0,0,0.001]]
+    # TODO refactor obstacle_list for lat lon.
+    # with open('obstacle_list.json') as obstacle_file:
+    #     obstacle_dict = json.load(obstacle_file)
+    #     obstacle_list = obstacle_dict['mlo_3']
+    obstacle_list = [[0, 0, 0.001]]
     # [x,y,size]
     # Set Initial parameters
-    # TODO refactor to NED
     path, rrt = ros_path_planning_node(start, goal, obstacle_list, 5.0)
     # Draw final path
     rrt.draw_graph()
